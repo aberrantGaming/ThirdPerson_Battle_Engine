554a14f3ae7fd6435d75181135a3287883e87c18
aberrantGeek <RyanCortino@gmail.com>
2018-01-23 06:59:14 -0600

    Implemented TimedEffects and the DamageOverTime class

---
+++ Assets/Assets/Scripts/TimedEffects/DamageOverTime.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageOverTime : TimedEffect 
+{
+    public int Damage;
+
+    protected override void ApplyEffect()
+    {
+        target.TakeDamage(Damage);
+    }
+}
---
+++ Assets/Assets/Scripts/TimedEffects/TimedEffect.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedEffect : MonoBehaviour
+{
+    #region Variables
+    public float Duration;      // when it should expire
+    public float StartTime;     // delay on the (first) effect tick
+    public float RepeatTime;    // time between each effect tick
+
+    [HideInInspector]
+    public EntityInterface.Entity target;
+    #endregion
+
+    // Use this for initialization
+    void Start ()
+    {
+        // Apply the effect repeated over time or direct
+        if (RepeatTime > 0)
+            InvokeRepeating("ApplyEffect", StartTime, RepeatTime);
+        else
+            Invoke("ApplyEffect", StartTime);
+
+        // End the effect accordingly
+        Invoke("EndEffect", Duration);
+    }
+	
+    protected virtual void ApplyEffect()
+    {
+
+    }
+
+    protected virtual void EndEffect()
+    {
+        CancelInvoke();
+        Destroy(gameObject);
+    }
+}
