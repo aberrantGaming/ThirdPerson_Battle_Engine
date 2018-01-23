9df370e9afd30490a1fc86f13d7b6d2ac4993f47
aberrantGeek <RyanCortino@gmail.com>
2018-01-23 06:08:06 -0600

    Began groundwork for self-cast abilities

---
+++ Assets/Assets/Scripts/Abilities/SelfcastAbility.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Abilities
+{
+    [CreateAssetMenu(menuName = "Ability/Selfcast Ability")]
+    public class SelfcastAbility : Ability
+    {
+        public SelfcastAbilityTriggerable scTrigger;
+
+        public override void Init(GameObject obj)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void TriggerAbility()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
